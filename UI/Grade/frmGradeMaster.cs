ffb2ec0926379baaf79e85258db3e8fb24dadcd0
PUVIshiyan <164120062+PUVIshiyan@users.noreply.github.com>
2024-07-22 10:57:43 +0530

    UI folder create (student file, subject file, grade file ) and login folder create

---
+++ UI/Grade/frmGradeMaster.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace School_management_system.UI.Grade
+{
+    public partial class frmGradeMaster : Form
+    {
+        public frmGradeMaster()
+        {
+            InitializeComponent();
+        }
+    }
+}
